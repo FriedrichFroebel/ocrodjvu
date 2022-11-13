e22e6f81245e8d2129aa6f9816096262d04d3f3c
FriedrichFroebel <FriedrichFroebel@users.noreply.github.com>
2022-11-13 20:24:38 +0100

    remove Python 2.7 version check

--- lib/__init__.py
+++ lib/__init__.py
@@ -1,8 +1,1 @@
-import sys
-
-if sys.version_info < (2, 7):  # no coverage
-    raise RuntimeError('Python 2.7 is required')
-if sys.version_info >= (3, 0):  # no coverage
-    raise RuntimeError('Python 2.X is required')
-
 # vim:ts=4 sts=4 sw=4 et
