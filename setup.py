291de9738840c5566f5fef9115cb964de4d6466f
FriedrichFroebel <FriedrichFroebel@users.noreply.github.com>
2023-01-15 17:29:26 +0100

    fix classifiers

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ setup(
         ],
     },
     classifiers=[
-        'Development Status :: 4 - Beta'
+        'Development Status :: 4 - Beta',
         'Environment :: Console',
         'Intended Audience :: End Users/Desktop',
         'License :: OSI Approved :: GNU General Public License (GPL)',
